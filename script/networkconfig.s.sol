57901cdbecb4d3ed4ce37d6b0149dc7aededa785
umar-ai <umarabbasi089@gmail.com>
2026-02-21 21:13:38 +0500

    mocks added for anvil based testing

--- script/networkconfig.s.sol
+++ script/networkconfig.s.sol
@@ -1,15 +1,18 @@
 //SPDX-License-Identifier:MIT
 pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/mockAgrregator.sol";
 
-contract networkConfig {
+contract networkConfig is Script {
     Networkconfig public activeNetworkConfig;
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
 
     constructor() {
         if (block.chainid == 11155111) {
             activeNetworkConfig = sepoliaNetworkConfig();
         } else {
-            activeNetworkConfig = sepoliaNetworkConfig();
+            activeNetworkConfig = anvilNetworkConfig();
         }
     }
 
@@ -22,5 +25,17 @@ contract networkConfig {
         return networkconfig;
     }
 
-    function anvilNetworkConfig() public pure returns (Networkconfig memory) {}
+    function anvilNetworkConfig() public returns (Networkconfig memory) {
+        // Check to see if we set an active network config
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
+        vm.stopBroadcast();
+
+        Networkconfig memory networkconfig = Networkconfig({priceFeed: address(mockPriceFeed)});
+        return networkconfig;
+    }
 }
