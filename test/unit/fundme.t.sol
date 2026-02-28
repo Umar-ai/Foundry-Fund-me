18907f4604c354bf2a1219b45099af88a6259b3a
umar-ai <umarabbasi089@gmail.com>
2026-02-28 10:55:56 +0500

    Deployed contract to sepolia and verification successfull

--- test/unit/fundme.t.sol
+++ test/unit/fundme.t.sol
@@ -3,8 +3,8 @@
 pragma solidity ^0.8.19;
 
 import {Test} from "forge-std/Test.sol";
-import {FundMe} from "../src/fund_me.sol";
-import {DeployFundme} from "../script/Deployfundme.s.sol";
+import {FundMe} from "../../src/fund_me.sol";
+import {DeployFundme} from "../../script/Deployfundme.s.sol";
 
 contract fundmeTest is Test {
     FundMe fundme;
